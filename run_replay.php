0d7deb189809ce30ce1648dc7d48d0dc314c91b6
Paul Cox <pdcox64@gmail.com>
2020-02-15 17:36:04 -0400

    Replay fixes

--- run_replay.php
+++ run_replay.php
@@ -1,22 +1,20 @@
 <?php
 
 include "db.php"; 
-include "incidents.php";
 include "logging.php";
+include "incidents.php";
+
 include "get_config.php";
 
 $sport = $_GET['sport'];
 $leagues = $_GET['leagues'];
 $start = $_GET['start'];
 $end = $_GET['end'];
+$replay = new stdClass;
 $error = 0;
+$total = 0;
 $output;
-$info;
 
-
-// ******************************************
-// *********** validate first ***************
-// ******************************************
 
 // get witnesses
 $witnesses = $config->subscriptions->witnesses;
@@ -26,32 +24,53 @@ $str_arr = explode ("|", $leagues);
 
 // do one league at a time
 foreach ($str_arr as $league)  {
-    // get the data
-    $data = array(); 
-    $game = '';
-    $error = 0;
-    $count=0;
-    $result = $con->query("SELECT * FROM vwgameevents WHERE  `sportname` = '$sport' AND `league` = '$league' 
-                            AND `status` = 0 AND `date` BETWEEN '$start' AND '$end'");
-
-    while ($obj = mysqli_fetch_array($result)) {
-            $incident = json_encode(make_incident($obj, $config),JSON_UNESCAPED_SLASHES);
-            $count++;
-            if(send_message($incident) == false){$error++;};
+        // get the data
+        $data = array(); 
+        $game = '';
+        $count=0;
+        $q = $con->query("SELECT * FROM vwgameevents WHERE  `sportname` = '$sport' AND `league` = '$league' 
+                                AND `status` = 0 AND `date` BETWEEN '$start' AND '$end'");
+        
+        while ($row=mysqli_fetch_object($q)){
+            $replay->call = "create";
+            $replay->sport = $row->sportname; 
+            $replay->league = $row->league; 
+            $replay->home = $row->hometeam;
+            $replay->away = $row->awayteam;
+            $replay->start_time = str_replace(' ', 'T',$row->datetime) . ":00.000Z";
+            $replay->match_id  = $row->gameid;
+            $incident = json_encode(make_incident($replay, $config),JSON_UNESCAPED_SLASHES); 
+          
+            if(send_message($incident) > 0 ){
+                $error++;
+               
+            }
+            else{
+                $count++;
+                $total++;
+            }
         } 
-        output_results($league,$count);  
+        output_results($league,$count);     
     }
-    if($error > 0){
-        $info = 'Incidents failed to send to all witness nodes - see error log';  
+    if($error > 0 || $total == 0){
+        $message->status = "400";
+        $message->title = "Failed to run replay for [" . $sport . "]";
+        $message->subcode = "445";
+        $message->message = "Replay failed to send to one or more endpoints";
     }
-    else($info = 'Success');
-    echo json_encode($sport .': {' . rtrim($output,', ') . '} info: ' . $info);
+    else{
+        $message->status = "200";
+        $message->title = "Replay completed";
+        $message->message = $sport .': {' . rtrim($output,', ') . '}';
+    }
+    echo json_encode($message);
 
 
 function send_message($incident){
     global $witnesses;
     $error = 0;
     // send incident message to BOS witnesses
+  
     foreach($witnesses as $witness) {
         
         $curl = curl_init(trim($witness->url)); // one witness for now
@@ -62,12 +81,11 @@ function send_message($incident){
         curl_setopt($curl, CURLOPT_POSTFIELDS, $incident);
         curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
         $curl_response = curl_exec($curl);
+        
         if ($curl_response === false) {
             $info = curl_getinfo($curl);
             curl_close($curl);
             $error++;
-            // log error
-            log_replay_error($info);
         }
         else{
             curl_close($curl);
@@ -75,16 +93,14 @@ function send_message($incident){
             if($decoded->id == null){
                $error++;
             }
-            else{ log_success($decoded, $witness);}
+            else{ 
+                //log_success($decoded, $witness);
+                //echo json_encode($decoded);
+            }
         }
     }
-    if($error > 0){
-        // at least one witness wasn't reached. 
-        return false;
-    }
-    else
-        return true;
-    }
+    return $error;
+}
 
     function output_results($league, $count){
         global $output;
