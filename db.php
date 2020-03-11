96bb842d31c4727db614f7a3d65337ac88384d40
roshansyed <r.syed@pbsa.info>
2020-03-11 20:07:10 -0300

    fix conflicts

--- db.php
+++ db.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     header('Access-Control-Allow-Origin: *');
     define('DIR_VENDOR', __DIR__.'/vendor/');
     if (file_exists(DIR_VENDOR . 'autoload.php')) {
@@ -15,9 +14,3 @@
 
     $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("could not connect DB");
 ?>
-=======
-    header('Access-Control-Allow-Origin: *');  
-    //$con = mysqli_connect("127.0.0.1", "root", "scoobydoo", "couch_potato") or die("could not connect DB"); 
-    $con = mysqli_connect("35.182.190.80", "paul", "scoobydoo", "couch_potato") or die("could not connect DB"); 
-?>
->>>>>>> c763ad889e3c0a3a649af505277dba58c66d67b8
